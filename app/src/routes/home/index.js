ee9245be73932c3440bad1fb127f8cbe1c0500c4
unknown <dbekdms@gmail.com>
2023-05-27 23:56:18 +0900

    로그인 화면 꾸미기

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,6 +7,8 @@ const ctrl = require("./home.ctrl");
 
 router.get("/",ctrl.output.hello);
 router.get("/login",ctrl.output.login);
+router.get("/register",ctrl.output.register);
 router.post("/login",ctrl.process.login);
+
 
 module.exports = router;//외부로 내보내는 명령어
