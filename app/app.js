efa9fefa43911d21f2ed1e95742876eafc58411c
unknown <dbekdms@gmail.com>
2023-05-27 16:41:37 +0900

    폴더 구조 최적화

--- app/app.js
+++ app/app.js
@@ -6,14 +6,12 @@ const app = express();
 const PORT = 3000;
 
 //라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 //앱 세팅
-app.set("views","./views");
+app.set("views","./src/views");
 app.set("view engine","ejs");
 
 app.use("/",home);//user-> 미들 웨어 등록해주는 메서드.
 
 module.exports=app;
-
-package.json
