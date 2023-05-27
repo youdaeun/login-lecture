b4a2267897a78e999e07bf5eb88fe9efac130b33
unknown <dbekdms@gmail.com>
2023-05-27 17:13:58 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,17 @@
 "use strict";
 
-console.log("hello");//login.js와 login.ejs가 연결되었는지 확인하는 코드, 프론트 단에서 동작하는 자바스크립트이다.
+//만약 console.log("hello");한다면 
+//이것의 의미는 login.js와 login.ejs가 연결되었는지 확인하는 코드, 프론트 단에서 동작하는 자바스크립트이다.
+
+const id = document.querySelector("#id"),//()안에는 선택자를 입력=public/ejs파일에 있는 정보
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");//tag 그대로 불러오는 코드
+
+loginBtn.addEventListener("click",login);
+
+function login(){
+    const req = {
+        id : id.value,
+        psword : psword.value,
+    };
+}
